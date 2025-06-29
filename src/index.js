6531b277667f71c94ced098277296c4b3dce9346
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-06-30 02:25:17 +0300

    fetching forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent page reload
+
+    const input = document.querySelector("input#searchByID");
+    const movieId = input.value;
+
+    fetch(`http://localhost:3000/movies/${movieId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Movie not found!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      })
+      .catch((error) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = "Not Found";
+        summary.innerText = error.message;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
